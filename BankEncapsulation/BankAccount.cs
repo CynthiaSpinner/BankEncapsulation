9d6b9318b6e41058dd640eaa3479fd4e3385548f
Cynthia <CynthiaSpinner@gmail.com>
2024-04-04 13:55:27 -0500

    in works for bonus

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+        public void Deposit(double amount)
+        {
+            _balance = _balance + amount;
+            Console.WriteLine($"Deposit Amount: +{amount}"); 
+        }
+
+        public double GetBalance() 
+        { 
+            Console.WriteLine($"Balance: {_balance}");
+            return _balance; 
+        }
+
+        public void Withdraw(double amount) 
+        {
+            _balance = _balance - amount;
+            Console.WriteLine($"Withdrawal Amount: -{amount}");
+        }
+
+        
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace BankEncapsulation
+﻿using System.ComponentModel;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine($"Welcome to Cynthia's Union Bank\n -------------------------------");
+
+            Console.WriteLine($"What would you like to do today?\n Deposit\t Withdraw\t Check Balance");
+
+            Console.ReadLine();
+
+            account.GetBalance();
+
+            account.Deposit(550.34);
+
+            account.GetBalance();
+
+            account.Withdraw(34.23);
+
+            account.GetBalance();
+            
         }
+
+     
     }
 }
